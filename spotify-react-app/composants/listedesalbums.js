b59bd365c62285719ce6e118d51368de4fde3566
jilani Belarbi <jilani.belarbi@epitech.eu>
2024-03-23 17:26:34 +0100

    Début des liste d'albums

---
+++ spotify-react-app/composants/listedesalbums.js
@@ -0,0 +1,23 @@
+import react, {useEffect, useState} from 'react';
+const listedesalbums = () => {
+    const [albums, definiralbums] = useState ([]); 
+
+    useEffect(() => {
+        fetch('http://localhost:8000/albums')
+        .then(reponse => reponse.json())
+        .then(donnees => definiralbums(donnees))
+        .catch(erreur => console.erreur("Problème Fetch: ",erreur));
+    }, []);
+
+    return (
+        <div>
+            <h1>Liste des Albums</h1>
+            <div>
+                {albums.map(album => (
+                    <div key={album.id}>
+                        <h2>{album.titre} - {album.artiste}</h2>
+                ))}
+            </div>
+        </div>
+    )
+}
