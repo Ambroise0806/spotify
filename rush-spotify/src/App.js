1cf20e400b080e6ffdf5f39500c919b009004de5
Arnaud LE BUNZE <arnaud.le-bunze@epitech.eu>
2024-03-23 18:23:15 +0100

    début de la page d'acceuil. Elle affiche maintenant 25 albums aleatoire (par page d'album et non par album). L'aleatoire ce passe au chargement de la page. Ya 0 style de fait (un peu mais rien de mis dans la pierre)

--- rush-spotify/src/App.js
+++ rush-spotify/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var rn = parseInt(getRandomArbitrary(1,65));
 
 function App() {
+  const [albums,setalbums] = useState([]);
+ 
+
+  async function fetchalbum(random_number) {
+    const response = await fetch('http://localhost:8000/albums?page='+random_number+'&limit=25');
+    const data = await response.json();
+    setalbums(data);
+  }
+
+  fetchalbum(rn);
+
+  // const albumLines = albums.length;
+
+  const albumLines = albums.map(album =>
+    <div key={album.id} className='div-album'>
+      <img src={album.cover_small}></img>
+      <div className='div-album-info'>
+        <h4>{album.name}</h4>
+        <h5>{album.artist_id}</h5 >
+        <h6>{album.release_date}</h6>
+      </div>
+    </div>
+  );
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {albumLines}
     </div>
   );
 }
