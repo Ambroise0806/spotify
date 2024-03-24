99c427d81b186cc5a408dea8e29a7db2d3e70614
Arnaud LE BUNZE <arnaud.le-bunze@epitech.eu>
2024-03-24 18:13:38 +0100

    page d'accueil de ma branch que j'ai adapté à la construction du la page fais par ambroise

--- rush-spotify/src/HomePage/HomePage.js
+++ rush-spotify/src/HomePage/HomePage.js
@@ -1,18 +1,56 @@
+import "./styles.css";
 import { Link } from "react-router-dom";
-import "./styles.css"
+import { useState } from 'react';
+
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+};
+
+var rn = parseInt(getRandomArbitrary(1, 163));
+
 
 export function HomePage() {
-    return (<div className="navbar">
-    <ul>
-      <li>
-        <Link to={`genres`}>Genres</Link>
-      </li>
-      <li>
-        <Link to={`artistes`}>Artistes</Link>
-      </li>
-      <li>
-        <Link to={`albums`}>Albums</Link>
-      </li>
-    </ul>
-  </div>)
+  const [albums, setalbums] = useState([]);
+
+  async function fetchalbum(random_number) {
+    const response = await fetch('http://localhost:8000/albums?page=' + random_number + '&limit=10');
+    const data = await response.json();
+    setalbums(data);
+  }
+
+  fetchalbum(rn);
+
+  const albumLines = albums.map(album =>
+    <div key={album.id} className='div-album'>
+      <img src={album.cover_small}></img>
+      <div className='div-album-info'>
+        <h3>{album.name}</h3>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="page">
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to={`genres`}>Genres</Link>
+          </li>
+          <li>
+            <Link to={`artistes`}>Artistes</Link>
+          </li>
+          <li>
+            <Link to={`albums`}>Albums</Link>
+          </li>
+          <li>
+            <Link to={`search`}>Recherche</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="content">
+        {albumLines}
+      </div>
+    </div>
+  );
 }
